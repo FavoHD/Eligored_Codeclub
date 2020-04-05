e8aa8bf21d17e8d9e1a4b8cc5e0a47d4cd1db972
FavoHD <hdfavo@gmail.com>
2020-04-05 16:07:55 +0200

    test

--- game/game.php
+++ game/game.php
@@ -10,7 +10,7 @@
     }else{
         $name = "Guest";
     }
-    
+
     if(isset($_SESSION["id"])) {
         $id = $_SESSION["id"];
     }else{
@@ -23,16 +23,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0">
         <title>Eligored</title>
-        
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link rel="stylesheet" href="../lib/hideWebhostBanner.css">
-        
-        
+
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" href="styles/game.css">
+		<link rel="stylesheet" href="../lib/hideWebhostBanner.css">
+
+
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-        
+
         <script type="text/javascript" src="../lib/collisionLib.min.js"></script>
         <script type="text/javascript" src="../lib/keyEventsLib.min.js"></script>
         <script type="text/javascript" src="../lib/mouseEventsLib.min.js"></script>
@@ -43,38 +44,38 @@
                 $string = "<script type='text/javascript'>";
                 $string.= "    window.location = '".$url."';";
                 $string.= "</script>";
-            
+
                 echo $string;
             }
-        
-        
+
+
             $content = " ";
             $site = "startMenu";
-            
+
             if ($site == "startMenu") {
                 if (isset($_GET["load_world"])) {
                     $error = false;
-                    
+
                     $world_id = $_GET["load_world"];
                     if ($world_id == -1) {
                         $world_id = $_POST["world_id"];
                     }
-                    
+
                     $site = "game";
                 }
-                
+
                 $content.= "<link rel='stylesheet' href='styles/startMenu.css'>
                             <div id='site' class='site'>
                                 <b>Name: ".$name."</b><br>
                                 <b>Id: ".$id."</b><br>
-                                
+
                                 <form action='?load_world=-1' method='post'>
                                     <input type='text' name='world_id' placeholder='World Id'>
-                                                
+
                                     <button type='submit'>Load World</button>
                                 </form>
                             ";
-                if ($id != 0) {                
+                if ($id != 0) {
                     $sql = "SELECT world_id, world_name FROM user_world WHERE user_id = $id";
                     foreach ($pdo->query($sql) as $row) {
                         $content.= "<form action='?load_world=".$row['world_id']."' method='post'>
@@ -83,7 +84,7 @@
                                     ";
                     }
                 }
-                                
+
                 $content.= "</div>
                             ";
             } else if ($site == "game") {
@@ -98,7 +99,7 @@
                             </div>
                             ";
             }
-                        
+
             echo $content;
         ?>
     </body>
