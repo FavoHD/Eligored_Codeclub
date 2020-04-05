61e9dd2b9598a4cabbf47ed9149686ea3141fb1a
FavoHD <hdfavo@gmail.com>
2020-04-05 16:46:53 +0200

    test

--- game/game.php
+++ game/game.php
@@ -22,7 +22,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0">
-        <title>Eligored</title>
+		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+		<title>Eligored</title>
 
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
